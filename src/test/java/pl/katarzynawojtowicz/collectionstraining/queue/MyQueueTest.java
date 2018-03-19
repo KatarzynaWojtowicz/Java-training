1dbe92ae47c1729206da5e863dedafa71d567f8a
KatarzynaWojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-03-19 22:13:18 +0100

    Created Unit Test For MyQueue

---
+++ src/test/java/pl/katarzynawojtowicz/collectionstraining/queue/MyQueueTest.java
@@ -0,0 +1,40 @@
+package pl.katarzynawojtowicz.collectionstraining.queue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyQueueTest {
+	@Test
+	public void shouldAddElementToEndOfQueue() throws Exception {
+
+		// Given
+		MyQueue<Integer> queue = new MyQueue<Integer>();
+		queue.add(5);
+		queue.add(8);
+
+		// When
+		queue.add(9);
+		String resultString = queue.toString();
+
+		// Then
+		Assert.assertEquals("[5, 8, 9]", resultString);
+	}
+
+	@Test
+	public void shouldRemoveFirstElement() throws Exception {
+
+		// Given
+		MyQueue<Integer> queue = new MyQueue<Integer>();
+		queue.add(5);
+		queue.add(8);
+		queue.add(1);
+
+		// When
+		queue.remove();
+
+		// Then
+		String resultString = queue.toString();
+		Assert.assertEquals("[8, 1]", resultString);
+	}
+
+}
