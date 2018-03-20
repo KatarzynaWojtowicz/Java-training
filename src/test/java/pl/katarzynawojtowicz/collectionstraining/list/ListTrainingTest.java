a797e1bd2bf4ed28213f192a5fdd7d8b6a36a865
KatarzynaWojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-03-20 22:39:08 +0100

    Added new Unit Tests for ListTraining

--- src/test/java/pl/katarzynawojtowicz/collectionstraining/list/ListTrainingTest.java
+++ src/test/java/pl/katarzynawojtowicz/collectionstraining/list/ListTrainingTest.java
@@ -24,4 +24,56 @@ public class ListTrainingTest {
 		String result = myList.toString();
 		Assert.assertEquals("[Dog, Cat, Rose]", result);
 	}
+
+	@Test
+	public void shouldReturnSize() throws Exception {
+
+		// Given
+		List<String> myList = new ArrayList<String>();
+		myList.add("Dog");
+		myList.add("Cat");
+		myList.add("Mouse");
+		myList.add("Rose");
+
+		// When
+		int result = myList.size();
+
+		// Then
+		Assert.assertEquals(4, result);
+	}
+
+	@Test
+	public void getElementOnIndex() throws Exception {
+
+		// Given
+		List<String> myList = new ArrayList<String>();
+		myList.add("Dog");
+		myList.add("Cat");
+		myList.add("Mouse");
+		myList.add("Rose");
+
+		// When
+		String result = myList.get(3);
+
+		// Then
+		Assert.assertEquals("Rose", result);
+	}
+
+	@Test
+	public void shouldRemoveElement() throws Exception {
+
+		// Given
+		List<String> myList = new ArrayList<String>();
+		myList.add("Dog");
+		myList.add("Cat");
+		myList.add("Mouse");
+		myList.add("Rose");
+
+		// When
+		myList.remove("Dog");
+		String result = myList.toString();
+
+		// Then
+		Assert.assertEquals("[Cat, Mouse, Rose]", result);
+	}
 }
