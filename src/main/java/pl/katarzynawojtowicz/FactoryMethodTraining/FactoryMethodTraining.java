f83bc3662bff70caf0c901b29a38f8db0654ad46
KatarzynaWojtowicz <wojtowicz.katarzyna.anna@gmail.com>
2018-03-28 14:00:18 +0200

    Created main class

--- src/main/java/pl/katarzynawojtowicz/FactoryMethodTraining/FactoryMethodTraining.java
+++ src/main/java/pl/katarzynawojtowicz/FactoryMethodTraining/FactoryMethodTraining.java
@@ -1,5 +1,10 @@
 package pl.katarzynawojtowicz.FactoryMethodTraining;
 
 public class FactoryMethodTraining {
-
+	public static void main(String args[]) {
+		ChocolateFactory factory = new ProducerFactor();
+		Chocolate milk = factory.prodChocolate(ChocolateType.MILK);
+		Chocolate dark = factory.prodChocolate(ChocolateType.DARK);
+		Chocolate withNuts = factory.prodChocolate(ChocolateType.NUTS);
+	}
 }
